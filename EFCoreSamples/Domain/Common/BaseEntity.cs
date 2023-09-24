13dc4751303c9c5823c3c292387eb082b7027d80
Deepak T <dthayyil@gmail.com>
2023-09-24 16:23:06 +0530

    Initial Commit

---
+++ EFCoreSamples/Domain/Common/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreSamples.Domain.Common
+{
+    public abstract class BaseEntity<T>
+    {
+         
+        public required T Id { get; set; }
+
+         
+    }
+
+}
---
+++ EFCoreSamples/Domain/Entities/Order.cs
@@ -0,0 +1,14 @@
+﻿using EFCoreSamples.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreSamples.Domain.Entities
+{
+    public class Order : BaseAuditableEntity<Guid>
+    {
+        
+    }
+}
